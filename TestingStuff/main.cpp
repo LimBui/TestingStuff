ca8add969ecc5fcbcbf2840523e2b21e240749ba
LimBui <limxbui@gmail.com>
2024-03-29 20:13:54 -0700

    Initial Commit

---
+++ TestingStuff/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TestingStuff
+//
+//  Created by LIMBO on 2/27/24.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int x, y;
+    x = 2;
+    y = 2*x++;
+    cout << x++ << y;
+
+}
